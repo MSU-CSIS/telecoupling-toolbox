72a2861480ee4244171c6b4d4ec570132772cfbc
Francesco Tonini <ftonini84@gmail.com>
2016-08-04 10:23:57 -0400

    added join field option

--- Scripts/DrawRadialFlows.py
+++ Scripts/DrawRadialFlows.py
@@ -14,32 +14,50 @@ def DrawRadialFlows():
     startY_field = arcpy.GetParameterAsText(2)
     endX_field = arcpy.GetParameterAsText(3)
     endY_field = arcpy.GetParameterAsText(4)
-    flowAmnt_field = arcpy.GetParameterAsText(5)
+    id_field = arcpy.GetParameterAsText(5)
     lineType_str = arcpy.GetParameterAsText(6)
-    SpRef = arcpy.GetParameterAsText(7)
+    spRef = arcpy.GetParameterAsText(7)
+    joinFields = arcpy.GetParameterAsText(8)
+    joinFields = joinFields.split(";")
 
     if inTable and inTable != "#":
 
         try:
-
             # XY To Line
             flowsOutputFC = os.path.join(arcpy.env.scratchGDB, "FlowLines")
             arcpy.AddMessage('Saved Flow Lines to: ' + flowsOutputFC)
             arcpy.SetProgressorLabel('Creating Radial Flow Lines ...')
-            if flowAmnt_field and flowAmnt_field != "#":
+            if id_field:
                 arcpy.XYToLine_management(in_table=inTable, out_featureclass=flowsOutputFC,
                                           startx_field=startX_field, starty_field=startY_field,
                                           endx_field=endX_field, endy_field=endY_field,
-                                          line_type=lineType_str, id_field=flowAmnt_field, spatial_reference=SpRef)
+                                          line_type=lineType_str, id_field=id_field, spatial_reference=spRef)
+
+                if joinFields[0] != '':
+                    ### IF any Join Fields are specified, then Copy Rows first and Join that to the input table ###
+                    arcpy.AddMessage('Creating Temporary Join Table ...')
+                    arcpy.SetProgressorLabel('Creating Temporary Join Table ...')
+                    ### Copy Rows from Input Table to make sure it has an OID ###
+                    outTable = r"in_memory\tempTable"
+                    arcpy.CopyRows_management(inTable, outTable)
+
+                    ### JOIN ###
+                    arcpy.AddMessage('Joining Selected Fields ...')
+                    arcpy.SetProgressorLabel('Joining Selected Fields ...')
+                    arcpy.JoinField_management(in_data=flowsOutputFC, in_field=id_field,
+                                               join_table=outTable, join_field=id_field,
+                                               fields=joinFields)
+                else:
+                    arcpy.AddWarning("WARNING: No join fields have been selected. Only the ID field will be copied to the output feature class!")
+
             else:
                 arcpy.XYToLine_management(in_table=inTable, out_featureclass=flowsOutputFC,
                                           startx_field=startX_field, starty_field=startY_field,
                                           endx_field=endX_field, endy_field=endY_field,
-                                          line_type=lineType_str, spatial_reference=SpRef)
+                                          line_type=lineType_str, spatial_reference=spRef)
 
             # Send string of (derived) output parameters back to the tool
-            arcpy.SetParameterAsText(8, flowsOutputFC)
-
+            arcpy.SetParameterAsText(9, flowsOutputFC)
             arcpy.ResetProgressor()
 
         except Exception:
--- Scripts/DrawRadialFlows_Nodes.py
+++ Scripts/DrawRadialFlows_Nodes.py
@@ -14,9 +14,11 @@ def DrawRadialFlows_Nodes():
     startY_field = arcpy.GetParameterAsText(2)
     endX_field = arcpy.GetParameterAsText(3)
     endY_field = arcpy.GetParameterAsText(4)
-    flowAmnt_field = arcpy.GetParameterAsText(5)
+    id_field = arcpy.GetParameterAsText(5)
     lineType_str = arcpy.GetParameterAsText(6)
-    SpRef = arcpy.GetParameterAsText(7)
+    spRef = arcpy.GetParameterAsText(7)
+    joinFields = arcpy.GetParameterAsText(8)
+    joinFields = joinFields.split(";")
 
     if inTable and inTable != "#":
 
@@ -24,23 +26,9 @@ def DrawRadialFlows_Nodes():
             # create empty list to append all output layers
             outList = []
 
-            # XY To Line
-            flowsOutputFC = os.path.join(arcpy.env.scratchGDB, "FlowLines")
-            arcpy.SetProgressorLabel('Creating Radial Flow Lines ...')
-            if flowAmnt_field and flowAmnt_field != "#":
-                arcpy.XYToLine_management(in_table=inTable, out_featureclass=flowsOutputFC,
-                                          startx_field=startX_field, starty_field=startY_field,
-                                          endx_field=endX_field, endy_field=endY_field,
-                                          line_type=lineType_str, id_field=flowAmnt_field, spatial_reference=SpRef)
-            else:
-                arcpy.XYToLine_management(in_table=inTable, out_featureclass=flowsOutputFC,
-                                          startx_field=startX_field, starty_field=startY_field,
-                                          endx_field=endX_field, endy_field=endY_field,
-                                          line_type=lineType_str, spatial_reference=SpRef)
-            outList.append(flowsOutputFC)
-
             # Make XY Event Layer (temporary)
             nodesOutput = "DestNodes"
+            arcpy.AddMessage('Creating Nodes at Flow Destinations ...')
             arcpy.SetProgressorLabel('Creating Nodes at Flow Destinations ...')
             arcpy.MakeXYEventLayer_management(table=inTable,
                                               in_x_field=endX_field, in_y_field=endY_field,
@@ -51,10 +39,45 @@ def DrawRadialFlows_Nodes():
             arcpy.CopyFeatures_management(in_features=nodesOutput, out_feature_class=nodesOutputFC)
             outList.append(nodesOutputFC)
 
+            # XY To Line
+            flowsOutputFC = os.path.join(arcpy.env.scratchGDB, "FlowLines")
+            arcpy.AddMessage('Saved Flow Lines to: ' + flowsOutputFC)
+            arcpy.SetProgressorLabel('Creating Radial Flow Lines ...')
+            if id_field:
+                arcpy.XYToLine_management(in_table=inTable, out_featureclass=flowsOutputFC,
+                                          startx_field=startX_field, starty_field=startY_field,
+                                          endx_field=endX_field, endy_field=endY_field,
+                                          line_type=lineType_str, id_field=id_field, spatial_reference=spRef)
+
+                if joinFields[0] != '':
+                    ### IF any Join Fields are specified, then Copy Rows first and Join that to the input table ###
+                    arcpy.AddMessage('Creating Temporary Join Table ...')
+                    arcpy.SetProgressorLabel('Creating Temporary Join Table ...')
+                    ### Copy Rows from Input Table to make sure it has an OID ###
+                    outTable = r"in_memory\tempTable"
+                    arcpy.CopyRows_management(inTable, outTable)
+
+                    ### JOIN ###
+                    arcpy.AddMessage('Joining Selected Fields ...')
+                    arcpy.SetProgressorLabel('Joining Selected Fields ...')
+                    arcpy.JoinField_management(in_data=flowsOutputFC, in_field=id_field,
+                                               join_table=outTable, join_field=id_field,
+                                               fields=joinFields)
+                else:
+                    arcpy.AddWarning("WARNING: No join fields have been selected. Only the ID field will be copied to the output feature class!")
+
+            else:
+                arcpy.XYToLine_management(in_table=inTable, out_featureclass=flowsOutputFC,
+                                          startx_field=startX_field, starty_field=startY_field,
+                                          endx_field=endX_field, endy_field=endY_field,
+                                          line_type=lineType_str, spatial_reference=spRef)
+
+            outList.append(flowsOutputFC)
+
+
             results = ";".join(outList)
             # Send string of (derived) output parameters back to the tool
-            arcpy.SetParameterAsText(8, results)
-
+            arcpy.SetParameterAsText(9, results)
             arcpy.ResetProgressor()
 
         except Exception:
