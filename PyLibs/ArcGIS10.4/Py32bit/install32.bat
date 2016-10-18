91818decd17eae556e12e3971f77a2377d11f232
Francesco Tonini <ftonini84@gmail.com>
2016-10-18 12:38:50 -0400

    added user guide & new PyLib batch installers

---
+++ PyLibs/ArcGIS10.4/Py32bit/install32.bat
@@ -0,0 +1,17 @@
+ECHO Adding 32-bit ArcGIS Python27 to system PATH...
+SET PATH=%PATH%C:\Python27\ArcGIS10.4;C:\Python27\ArcGIS10.4\Scripts
+ECHO Installing Required Python Modules...
+pip install GDAL-1.11.3-cp27-none-win32.whl
+pip install matplotlib-1.5.3-cp27-cp27m-win32.whl
+pip install numpy-1.11.2+mkl-cp27-cp27m-win32.whl
+pip install scipy-0.18.1-cp27-cp27m-win32.whl
+pip install pyamg-2.3.2-cp27-none-win32.whl
+pip install Rtree-0.8.2-cp27-cp27m-win32.whl
+pip install Shapely-1.5.17-cp27-cp27m-win32.whl
+pip install natcap.versioner
+pip install setuptools
+pip install pygeoprocessing-0.3.0a18-cp27-none-win32.whl
+pip install natcap.invest
+pip install bs4
+pip install BeautifulSoup
+PAUSE
