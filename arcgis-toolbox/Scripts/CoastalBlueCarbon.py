3e874ec250b8a612f1f4384d97a7128617b7b8be
Rui Zhang <zhangr50@msu.edu>
2018-09-27 12:18:17 -0400

    modified cbc script but not working yet; add cbc toolbox

--- arcgis-toolbox/Scripts/CoastalBlueCarbon.py
+++ arcgis-toolbox/Scripts/CoastalBlueCarbon.py
@@ -9,60 +9,62 @@ import natcap.invest.coastal_blue_carbon.coastal_blue_carbon
 arcpy.env.overwriteOutput = True
 workspace_dir = arcpy.env.scratchFolder
 
-#List of output files
 
-_OUTPUT = {
-    'rkls': r'rkls.tif',
-    'sed_export': r'sed_export.tif',
-    'sed_retention_index': r'sed_retention_index.tif',
-	'stream': r'stream.tif',
-	'usle': r'usle.tif',
-	'water_result_sdr': r'water_result_sdr.shp',
-	'sed_retention': r'sed_retention.tif'
-}
 
 #Get the arguments to include in the invest.sdr model run
 def GetArgs():
-	biophysical_table = arcpy.GetParameterAsText(0)
-	dem = arcpy.GetParameterAsText(1)
-	erodibility = arcpy.GetParameterAsText(2)
-	erosivity = arcpy.GetParameterAsText(3)
-	IC_param = arcpy.GetParameterAsText(4)
-	K_param = arcpy.GetParameterAsText(5)
-	lulc = arcpy.GetParameterAsText(6)
-	sdr_max = arcpy.GetParameterAsText(7)
-	threshold_flow_acc = arcpy.GetParameterAsText(8)
-	watersheds = arcpy.GetParameterAsText(9)
-	#new
-	drainage_path = arcpy.GetParameterAsText(10)
-	#new
-	#drainage_file = arcpy.GetParameterAsText(11)
+	lulc_lookup_table = arcpy.GetParameterAsText(0)
+	lulc_transition_effect_of_carbon_table = arcpy.GetParameterAsText(1)
+	carbon_pool_initial_variables_table = arcpy.GetParameterAsText(2)
+	carbon_pool_transient_variables_table = arcpy.GetParameterAsText(3)
+	baseline_raster = arcpy.GetParameterAsText(4)
+	year_of_baseline = arcpy.GetParameterAsText(5)
+	transition_lulc_raster_list = arcpy.GetParameterAsText(6)
+	transition_year_list = arcpy.GetParameterAsText(7)
+	do_economic_analysis = arcpy.GetParameterAsText(8)
+	use_price_table = arcpy.GetParameterAsText(9)
+	price = arcpy.GetParameterAsText(10)
+	interest_rate = arcpy.GetParameterAsText(11)
+	discount_rate = arcpy.GetParameterAsText(12)
+	price_table = arcpy.GetParameterAsText(13)
+	analysis_year = arcpy.GetParameterAsText(14)
 	
-	outRast = []
+	outList = []
 	
 	try:
 	
 		args = {
-			u'biophysical_table_path': biophysical_table,
-			u'dem_path': dem,
-			#edited
-			#u'drainage_path': u''
-			#u'drainage_path': False,
-			u'erodibility_path': erodibility,
-			u'erosivity_path': erosivity,
-			u'ic_0_param': IC_param,
-			u'k_param': K_param,
-			u'lulc_path': lulc,
-			u'sdr_max': sdr_max,
-			u'threshold_flow_accumulation': threshold_flow_acc,
-			u'watersheds_path': watersheds,
-			u'workspace_dir': workspace_dir
+			#u'analysis_year': year_of_baseline,
+			u'carbon_pool_initial_uri': carbon_pool_initial_variables_table,
+			u'carbon_pool_transient_uri': carbon_pool_transient_variables_table,
+			#u'discount_rate': discount_rater,
+			u'do_economic_analysis': do_economic_analysis,
+			u'do_price_table': use_price_table,
+			#u'interest_rate': interest_rate,
+			u'lulc_baseline_map_uri': baseline_raster,
+			u'lulc_baseline_year': year_of_baseline,
+			u'lulc_lookup_uri': lulc_lookup_table,
+			u'lulc_transition_maps_list': transition_lulc_raster_list,
+			u'lulc_transition_matrix_uri': lulc_transition_effect_of_carbon_table,
+			u'lulc_transition_years_list': transition_year_list,
+			u'workspace_dir': workspace_dir,
+			
 		}
 		
-		#new
-		if drainage_path:
-			args[u'drainage_path'] = drainage_path
-			#args[u'drainage_file'] = drainage_file
+		
+		if do_economic_analysis:
+			if use_price_table:
+				args[u'price_table_uri'] = price_table
+			else:
+				args[u'discount_rate'] = discount_rate
+				args[u'interest_rate'] = interest_rate
+				args[u'price'] = price
+		
+		if analysis_year:
+			args[u'analysis_year'] = analysis_year
+		
+		
+			
 		
 	except Exception:
 		e = sys.exc_info()[1]
@@ -70,32 +72,38 @@ def GetArgs():
 		
 	return args
 	
+#List of output files
+
+_OUTPUT = {
+    'carbon_stock': 'carbon_stock_at_%s.tif',
+    'carbon_accumulation': 'carbon_accumulation_between_%s_and_%s.tif',
+    'cabon_emissions': 'carbon_emissions_between_%s_and_%s.tif',
+    'carbon_net_sequestration': 'net_carbon_sequestration_between_%s_and_%s.tif',
+	'net_present_value': 'net_present_value.tif',
+	'total_net_carbon_sequestration': 'total_net_carbon_sequestration.tif',
+}
+	
 if __name__ == '__main__':
 	args = GetArgs()
 	
 	#Run the Nat Cap module
-	natcap.invest.sdr.execute(args)
+	natcap.invest.coastal_blue_carbon.coastal_blue_carbon.execute(args)
 	
 	#Assign Nat Cap model output to variables
-	rkls = os.path.join(arcpy.env.scratchFolder, _OUTPUT['rkls'])
-	sed_export = os.path.join(arcpy.env.scratchFolder, _OUTPUT['sed_export'])
-	sed_retention_index = os.path.join(arcpy.env.scratchFolder, _OUTPUT['sed_retention_index'])
-	stream = os.path.join(arcpy.env.scratchFolder, _OUTPUT['stream'])
-	usle = os.path.join(arcpy.env.scratchFolder, _OUTPUT['usle'])
-	water_result_sdr = os.path.join(arcpy.env.scratchFolder, _OUTPUT['water_result_sdr'])
-	sed_retention = os.path.join(arcpy.env.scratchFolder, _OUTPUT['sed_retention'])
-	
-	#Define the projection of each of the output files
-	#defProj(args[u'dem_raster_path'], b_rast, b_sum_rast, cn_rast, l_rast, l_avail_rast, l_sum_rast, l_sum_avail_rast, qf_rast, vri_rast, agg_results_shp)
+	carbon_stock = os.path.join(arcpy.env.scratchFolder, _OUTPUT['carbon_stock'])
+	carbon_accumulation = os.path.join(arcpy.env.scratchFolder, _OUTPUT['carbon_accumulation'])
+	carbon_emissions = os.path.join(arcpy.env.scratchFolder, _OUTPUT['carbon_emissions'])
+	carbon_net_sequestration = os.path.join(arcpy.env.scratchFolder, _OUTPUT['carbon_net_sequestration'])
 	
 	#Remove the intermediate folder
 	shutil.rmtree(os.path.join(arcpy.env.scratchFolder, 'intermediate_outputs'))
 	
 	#Add outputs to map viewer
-	arcpy.SetParameter(11, rkls)
-	arcpy.SetParameter(12, sed_export)
-	arcpy.SetParameter(13, sed_retention_index)
-	arcpy.SetParameter(14, stream)
-	arcpy.SetParameter(15, usle)
-	arcpy.SetParameter(16, water_result_sdr)
-	arcpy.SetParameter(17, sed_retention)
+	arcpy.SetParameter(15, carbon_stock)
+	arcpy.SetParameter(16, carbon_accumulation)
+	arcpy.SetParameter(17, carbon_emissions)
+	arcpy.SetParameter(18, carbon_net_sequestration)
+	arcpy.SetParameter(19, net_present_value)
+	arcpy.SetParameter(20, total_net_carbon_sequestration)
+	
+	
