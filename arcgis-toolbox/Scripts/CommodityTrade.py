c1261685c04b1228d8111114edddc5d178663c75
pamccord <pamccord@indiana.edu>
2018-11-21 15:34:19 -0500

    edited script to include geometry information

--- arcgis-toolbox/Scripts/CommodityTrade.py
+++ arcgis-toolbox/Scripts/CommodityTrade.py
@@ -21,8 +21,9 @@ def CommodityTrade():
 	commodityItem = arcpy.GetParameterAsText(3) #select commodity of interest from list
 	startYear = arcpy.GetParameterAsText(4) #start year of analysis
 	endYear = arcpy.GetParameterAsText(5) #end year of analysis
-	limit = arcpy.GetParameterAsText(6) #gives users the ability to limit trade partners
-	numLimit = arcpy.GetParameterAsText(7) #the number of trade partners to limit output to
+	outputName = arcpy.GetParameterAsText(6) #name of output feature class
+	limit = arcpy.GetParameterAsText(7) #gives users the ability to limit trade partners
+	numLimit = arcpy.GetParameterAsText(8) #the number of trade partners to limit output to
 	
 	try:
 		#make sure end year follows the start year
@@ -59,10 +60,61 @@ def CommodityTrade():
 				df_trade = df_trade.groupby(["year"], sort = False).apply(lambda x: x.sort_values(["import_val"], ascending = False)) #group data by year and sort descending by import_val 
 				df_trade = df_trade.groupby("year").head(numLimit).reset_index(drop=True) #Take only as many top trading partners as specified in numLimit
 		
-		#testing xy to line
-		arcpy.AddMessage("here1")
-		arcpy.XYToLine_management(df_trade, "C:\\Users\\Paul McCord\\Google Drive\\Toolbox\\Tool_First\\CommodityTrade\\testOutput\\test1.shp", 
-									"long_origin", "lat_origin", "long_dest", "lat_dest", "GEODESIC")
+		#create feature class for commodity output
+		wgs = arcpy.SpatialReference(4326)
+		fc = arcpy.CreateFeatureclass_management(arcpy.env.scratchFolder, outputName, "Polyline", "", "", "", wgs)
+		arcpy.AddField_management(fc, "Year", "SHORT")  #add year field
+		arcpy.AddField_management(fc, "Value", "DOUBLE")  #add export/import value field
+		arcpy.AddField_management(fc, "Commodity", "TEXT")  #add commodity name field
+		arcpy.AddField_management(fc, "Origin", "TEXT")  #add flow origin field
+		arcpy.AddField_management(fc, "Dest", "TEXT") #add flow destination field
+		
+		arcpy.AddMessage("created fc with column names")
+		
+		#populate feature class with geographic and commodity trade information
+		#establish cursor
+		cursor = arcpy.InsertCursor(fc)
+		
+		arcpy.AddMessage("established cursor")
+		
+		#direction == "Export"
+		#flows are drawn by 1) plotting origin and destination points, then 2) drawing line between points
+		if direction == "Export":
+			point = arcpy.Point()
+			array = arcpy.Array()
+			featureList = []
+			feat = cursor.newRow()
+			for i, row in df_trade.iterrows():
+				point.X = row["long_origin"]
+				point.Y = row["lat_origin"]
+				array.add(point)
+				point.X = row["long_dest"]
+				point.Y = row["lat_dest"]
+				array.add(point)
+				polyline = arcpy.Polyline(array)
+				array.removeAll()
+				featureList.append(polyline)
+				feat.shape = polyline
+				cursor.insertRow(feat)
+			del feat
+			del cursor
+			
+			arcpy.AddMessage("geometry added to fc")
+			
+			#add year, value, commodity name, origin, and destination information
+			j = 0
+			with arcpy.da.UpdateCursor(fc, ("Year", "Value", "Commodity", "Origin", "Dest")) as cursor:
+				for ROW in cursor:
+					ROW[0] = df_trade["year"][j]
+					ROW[1] = df_trade["export_val"][j]
+					ROW[2] = df_trade["comm_name"][j]
+					ROW[3] = df_trade["country_origin"][j]
+					ROW[4] = df_trade["country_dest"][j]
+					cursor.updateRow(ROW)
+					j += 1
+			
+			arcpy.AddMessage("commodity trade information added")
+			
 		arcpy.AddMessage("here2")
 	
 	except Exception:
